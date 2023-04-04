d8f3648876df2bcb3e365a3b0ff58402348483e1
Trioo12 <prawironegorotrio@gmail.com>
2023-04-04 20:20:53 +0700

    tugas php8 pertemuan 9

---
+++ tugasphp8.php/about.php
@@ -0,0 +1,122 @@
+
+<ol class="breadcrumb mb-4">
+    <li class="breadcrumb-item"><a href="index.php?url=dashboard">Dashboard</a></li>
+    <li class="breadcrumb-item active">Tables</li>
+</ol>
+<div class="card mb-4">
+    <div class="card-body">
+       Informasi Mengenai Pegawai Politeknik Negeri Malang pada tahun 2023 <br>
+        <a target="_blank" href="https://www.polinema.ac.id/">www.polinema.ac.id</a>
+        .
+    </div>
+</div>
+<div class="card mb-4">
+    <div class="card-header">
+        <i class="fas fa-table me-1"></i>
+        About    
+</div>
+
+
+<head>
+	<title>Tentang Pegawai</title>
+	<style>
+		table {
+			border-collapse: collapse;
+			width: 100%;
+		}
+		th, td {
+			padding: 8px;
+			text-align: left;
+			border-bottom: 1px solid #ddd;
+		}
+		th {
+			background-color: #4CAF50;
+			color: white;
+		}
+	</style>
+</head>
+<body>
+	<h1>Tentang Pegawai</h1>
+	<table>
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Position</th>
+				<th>Email</th>
+                <th>Gaji</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Trio Prawiro Negoro</td>
+				<td>CEO</td>
+				<td>prawironegorotrio@gmail.com</td>
+                <td>$10,000,000</td>
+			</tr>
+			<tr>
+				<td>Siti Nur Khalifah</td>
+				<td>Sekretaris Manager</td>
+				<td>sitinurkhalifah@gmail.com</td>
+                <td>$8,000,000</td>
+			</tr>
+			<tr>
+				<td>jojon Santoso</td>
+				<td>Pelayan</td>
+				<td>jojonsantoso@yahoo.co.id</td>
+                <td>$4,000,000</td>
+			</tr>
+            <tr>
+				<td>Muayyad Sakinah</td>
+				<td>Pelayan</td>
+				<td>Muayyadsakinah@yahoo.co.id</td>
+                <td>$4,000,000</td>
+			</tr>
+            <tr>
+				<td>Nirmala nuaiman</td>
+				<td>Pelayan</td>
+				<td>Nirmalanuaiman@yahoo.co.id</td>
+                <td>$4,000,000</td>
+			</tr>
+            <tr>
+				<td>Suep Jaidi</td>
+				<td>Pelayan</td>
+				<td>Suepjaidi@yahoo.co.id</td>
+                <td>$4,000,000</td>
+			</tr>
+            <tr>
+				<td>Fadil mugharib</td>
+				<td>Pelayan</td>
+				<td>Fadil mugharib@yahoo.co.id</td>
+                <td>$4,000,000</td>
+			</tr>
+            <tr>
+				<td>Sulaiman Farisi</td>
+				<td>Pelayan</td>
+				<td>Sulaimanfarisi@yahoo.co.id</td>
+                <td>$4,000,000</td>
+			</tr>
+            <tr>
+				<td>Santoso</td>
+				<td>Pelayan</td>
+				<td>santoso@yahoo.co.id</td>
+                <td>$4,000,000</td>
+			</tr>
+            <tr>
+				<td>Adiba</td>
+				<td>Pelayan</td>
+				<td>adiba@yahoo.co.id</td>
+                <td>$4,000,000</td>
+			</tr>
+            <tr>
+				<td>jijin</td>
+				<td>Pelayan</td>
+				<td>jijin@yahoo.co.id</td>
+                <td>$4,000,000</td>
+			</tr>
+          
+		</tbody>
+	</table>
+</body>
+</div>
+</div>
+
